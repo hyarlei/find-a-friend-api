2f8accf5b8c8a6b9c6ba73d8bc00282286013885
Hyarlei Silva <hyarleysf@gmail.com>
2025-12-27 11:53:45 -0300

    feat: add Swagger

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,44 @@
 import fastifyJwt from "@fastify/jwt"
+import fastifySwagger from "@fastify/swagger"
+import fastifySwaggerUi from "@fastify/swagger-ui"
 import fastify from "fastify"
+import {
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+  ZodTypeProvider,
+} from "fastify-type-provider-zod"
 import { env } from "./env"
 import { appRoutes } from "./http/routes"
 
-export const app = fastify()
+export const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: "Find A Friend API",
+      description: "API para adoção de animais",
+      version: "1.0.0",
+    },
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT",
+        },
+      },
+    },
+  },
+  transform: jsonSchemaTransform,
+})
+
+app.register(fastifySwaggerUi, {
+  routePrefix: "/docs",
+})
 
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET!,
--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,14 +1,84 @@
 import { FastifyInstance } from "fastify"
+import { ZodTypeProvider } from "fastify-type-provider-zod"
+import z from "zod"
 import { authenticate } from "./controllers/authenticate"
-import { createPet } from "./controllers/create-pet"
 import { getPetProfile } from "./controllers/get-pet-profile"
 import { register } from "./controllers/register"
 import { searchPets } from "./controllers/search-pets"
 
 export async function appRoutes(app: FastifyInstance) {
-  app.post("/orgs", register)
-  app.post("/sessions", authenticate)
-  app.post("/pets", createPet)
-  app.get("/pets/:id", getPetProfile)
-  app.get("/pets", searchPets)
+  const typedApp = app.withTypeProvider<ZodTypeProvider>()
+
+  typedApp.post(
+    "/orgs",
+    {
+      schema: {
+        tags: ["ORGs"],
+        summary: "Create a new ORG",
+        body: z.object({
+          name: z.string(),
+          email: z.string().email(),
+          password: z.string().min(6),
+          address: z.string(),
+          phone: z.string(),
+        }),
+        response: {
+          201: z.null(),
+        },
+      },
+    },
+    register,
+  )
+
+  typedApp.post(
+    "/sessions",
+    {
+      schema: {
+        tags: ["AUTH"],
+        summary: "Authenticate (Login)",
+        body: z.object({
+          email: z.string().email(),
+          password: z.string().min(6),
+        }),
+        response: {
+          200: z.object({
+            token: z.string(),
+          }),
+        },
+      },
+    },
+    authenticate,
+  )
+
+  typedApp.post(
+    "/pets",
+    {
+      schema: {
+        tags: ["PETS"],
+        summary: "Search for pets",
+        querystring: z.object({
+          city: z.string(),
+          age: z.string().optional(),
+          size: z.string().optional(),
+          energy_level: z.string().optional(),
+        }),
+        response: {
+          200: z.object({
+            pets: z.array(
+              z.object({
+                id: z.string().uuid(),
+                name: z.string(),
+                age: z.string(),
+                size: z.string(),
+                energy_level: z.string(),
+              }),
+            ),
+          }),
+        },
+      },
+    },
+    searchPets,
+  )
+  typedApp.get("/pets/:id", getPetProfile)
+  typedApp.get("/pets", searchPets)
 }
