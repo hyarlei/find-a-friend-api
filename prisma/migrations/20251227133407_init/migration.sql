c7da5898fb4fb81a08db3bed83417be267516425
Hyarlei Silva <hyarleysf@gmail.com>
2025-12-27 10:39:46 -0300

    init migration

--- prisma/migrations/20251227133407_init/migration.sql
+++ prisma/migrations/20251227133407_init/migration.sql
@@ -1,17 +1,19 @@
 -- CreateTable
 CREATE TABLE "orgs" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "password_hash" TEXT NOT NULL,
     "address" TEXT NOT NULL,
     "phone" TEXT NOT NULL,
-    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "orgs_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "pets" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "about" TEXT,
     "age" TEXT NOT NULL,
@@ -20,8 +22,12 @@ CREATE TABLE "pets" (
     "independence_level" TEXT NOT NULL,
     "environment" TEXT NOT NULL,
     "org_id" TEXT NOT NULL,
-    CONSTRAINT "pets_org_id_fkey" FOREIGN KEY ("org_id") REFERENCES "orgs" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+
+    CONSTRAINT "pets_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
 CREATE UNIQUE INDEX "orgs_email_key" ON "orgs"("email");
+
+-- AddForeignKey
+ALTER TABLE "pets" ADD CONSTRAINT "pets_org_id_fkey" FOREIGN KEY ("org_id") REFERENCES "orgs"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
